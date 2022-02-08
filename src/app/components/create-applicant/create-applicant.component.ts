cb8b6721f3d2b895f46eed389a395e93fac86f23
Natalia Gonzalez <73859972+Nataliasita@users.noreply.github.com>
2022-02-08 17:05:12 -0500

    ajustes formulario

--- src/app/components/create-applicant/create-applicant.component.ts
+++ src/app/components/create-applicant/create-applicant.component.ts
@@ -10,6 +10,21 @@ import { ApplicantService } from 'src/app/services/applicant.service';
 })
 export class CreateApplicantComponent implements OnInit {
 
+  optionsDoc= ['Cedula de Ciudadania', 'Cedula Extranjeria','Pasaporte'];
+
+  optionStudyLevel= ['Basica secundaria','Tecnico o Tecnologo','Pregrado Universitario','Posgrado - Especializacion','Posgrado - Maestria','Posgrado - Doctorado'];
+
+  optionState =['Terminado','En curso','Abandonado','Aplazado'];
+
+  optionTime =['Menos de 1 año','Entre 1 año a 2 años','Entre 3 años a 4 años','Mas de 5 años'];
+
+  optionCompany=['Administracion','Agricultura y desarrollo rural', 'Comercial', 'Energetico','Financiero',
+                  'Industrial','Tecnologia y software','Turistico', 'Otro'
+                ]
+                
+  optionMonth=['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic',]
+
+
   createApplicant:FormGroup;
   submitted=false;
 
@@ -22,6 +37,9 @@ export class CreateApplicantComponent implements OnInit {
       numberIdent:['', Validators.required],
       email:['', Validators.required],
       phoneContact:['', Validators.required],
+      country:['', Validators.required],
+      city:['', Validators.required],
+      levelStudy:['', Validators.required],
     })
   }
 
@@ -41,6 +59,9 @@ export class CreateApplicantComponent implements OnInit {
       numberIdent:this.createApplicant.value.numberIdent,
       email:this.createApplicant.value.email,
       phoneContact:this.createApplicant.value.phoneContact,
+      country:this.createApplicant.value.country,
+      city:this.createApplicant.value.city,
+      levelStudy:this.createApplicant.value.levelStudy,
       dateCreation: new Date(),
       dateUpgrade: new Date(),
     }
